d3de371ce9d26264b7816680338aa8c831a77dd3
Ky Lee <hello@kyhy.co>
2025-09-19 18:04:30 -0400

    fix autosuggestion color

--- kysh.zsh
+++ kysh.zsh
@@ -8,6 +8,9 @@ source "$KYSH_PATH/powerlevel10k-theme.zsh"
 source "$KYSH_PATH/completions/gh.zsh"
 source "$KYSH_PATH/completions/fh.zsh"
 
+# zsh-autosuggestions color (uses terminal theme's muted/comment color)
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=black,bold'
+
 # External
-source "$KYSH_PATH/external/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 source "$KYSH_PATH/external/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "$KYSH_PATH/external/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
